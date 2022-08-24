456d7bfdb14345ffbf118f6c459680200aab3ed2
al-bur <64825713+al-bur@users.noreply.github.com>
2022-08-24 21:04:12 +0900

    feat(mission): 미션 목록 조회, 생성 기능 구현 (#3)

---
+++ src/components/Missions.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Datagrid, List, NumberField, TextField } from 'react-admin';
+
+const Missions = () => (
+  <List>
+    <Datagrid>
+      <TextField source="id" />
+      <TextField source="name" />
+      <NumberField source="session.id" />
+      <TextField source="session.name" />
+    </Datagrid>
+  </List>
+);
+
+export default Missions;
