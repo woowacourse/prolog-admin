ef8e834b7ed2706803301225fcd4d964d8a2d755
al-bur <64825713+al-bur@users.noreply.github.com>
2022-08-31 13:44:27 +0900

    feat(session): 세션 목록 조회 및 생성 기능 구현 (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import MissionCreate from './components/MissionCreate';
 import Missions from './components/Missions';
+import Sessions from './components/Sessions';
+import SessionCreate from './components/SessionCreate';
 
 const dataProvider = jsonServerProvider(`${process.env.REACT_APP_API_URL}`);
 
@@ -10,6 +12,7 @@ function App() {
   return (
     <Admin dataProvider={dataProvider}>
       <Resource name="missions" list={Missions} create={MissionCreate} />
+      <Resource name="sessions" list={Sessions} create={SessionCreate} />
     </Admin>
   );
 }
