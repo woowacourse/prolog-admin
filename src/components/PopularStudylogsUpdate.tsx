a0d5c640f2ae01bd9cb9de321529b5b08f381a58
al-bur <64825713+al-bur@users.noreply.github.com>
2022-11-10 21:49:50 +0900

    feat: 인기있는 학습로그 업데이트 기능 구현

---
+++ src/components/PopularStudylogsUpdate.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const PopularStudylogsUpdate = () => {
+  const updatePopularStudylogs = () => {
+    if (window.confirm('인기있는 학습로그를 업데이트 하시겠습니까?')) {
+      fetch(`${process.env.REACT_APP_API_URL}/studylogs/popular/sync`).catch(
+        (err) => console.log(err)
+      );
+    }
+  };
+
+  return (
+    <Button variant="contained" onClick={updatePopularStudylogs}>
+      인기있는 학습로그 업데이트
+    </Button>
+  );
+};
+
+export default PopularStudylogsUpdate;
