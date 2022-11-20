6b11e1ef199aa96d0cc570fc95bf52308b6ed462
Marco <bigdatajang@gmail.com>
2022-11-21 03:16:45 +0900

    refactor: mui에 대한 import 방법

--- src/customPages/RoadmapSelectTopKeywordPage/components/TopKeywordList/index.tsx
+++ src/customPages/RoadmapSelectTopKeywordPage/components/TopKeywordList/index.tsx
@@ -1,11 +1,13 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from '@mui/material';
 import { TopKeywordListProps } from './type';
 
 const TopKeywordList = ({ rows, onClickMove }: TopKeywordListProps) => {
