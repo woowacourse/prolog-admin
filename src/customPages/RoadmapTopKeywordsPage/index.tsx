a8a7ff0ff331403b57ae58c8a2c1223035d18047
gytks4 <gytks4@naver.com>
2022-12-02 04:05:16 +0900

    fix: 응답값이 없더라도 키워드를 추가하는 기능은 동작하도록 수정

--- src/customPages/RoadmapTopKeywordsPage/index.tsx
+++ src/customPages/RoadmapTopKeywordsPage/index.tsx
@@ -14,17 +14,15 @@ const RoadmapTopKeywordsPage = () => {
 
   const { open, openModal, closeModal } = useModal();
 
-  if (typeof childrenKeywordList === 'undefined') {
-    return <h3>선택하신 최상위 키워드에 콘텐츠가 존재하지 않습니다.</h3>;
-  }
-
   return (
     <div>
       <h2>최상위 키워드 수정/삭제</h2>
-      <SubKeywordList
-        childrenKeywordList={childrenKeywordList}
-        sessionId={Number(sessionId)}
-      />
+      {childrenKeywordList && (
+        <SubKeywordList
+          childrenKeywordList={childrenKeywordList}
+          sessionId={Number(sessionId)}
+        />
+      )}
       <br />
       <Button
         onClick={openModal}
