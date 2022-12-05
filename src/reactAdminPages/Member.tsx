6f55ee4aaac51d807c606bd1c275a422a5db6272
JUDONGHYEOK <judonghyeok@gmail.com>
2022-12-05 17:38:34 +0900

    feat: 등업 성공시 refetch

--- src/reactAdminPages/Member.tsx
+++ src/reactAdminPages/Member.tsx
@@ -3,36 +3,42 @@ import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import { useGetMembers } from '../hooks/members';
 import { client } from '../client';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'username', headerName: 'User Name', width: 200 },
-  { field: 'nickname', headerName: 'Nickname', width: 180 },
-  {
-    field: 'role',
-    headerName: 'Role',
-    width: 90,
-  },
-  {
-    field: 'progress',
-    headerName: 'progress',
-    sortable: false,
-    filterable: false,
-    renderCell: (param: GridRenderCellParams) => (
-      <Button
-        variant="outlined"
-        onClick={() => {
-          client.post(`/members/${param.row.id}/promote/approve`);
-        }}
-      >
-        등업
-      </Button>
-    ),
-  },
-];
+
 
 const Members = () => {
-  const { data: users, isLoading, isError } = useGetMembers();
-
+  const { data: users, isLoading, isError,refetch } = useGetMembers();
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'username', headerName: 'User Name', width: 200 },
+    { field: 'nickname', headerName: 'Nickname', width: 180 },
+    {
+      field: 'role',
+      headerName: 'Role',
+      width: 90,
+    },
+    {
+      field: 'progress',
+      headerName: 'progress',
+      sortable: false,
+      filterable: false,
+      renderCell: (param: GridRenderCellParams) => (
+        <Button
+          variant="outlined"
+          onClick={() => {
+            try{
+              client.post(`/members/${param.row.id}/promote/approve`);
+              alert('등업에 성공하였습니다.');
+              refetch();
+            }catch(e:any){
+              alert(e.message);
+            }
+          }}
+        >
+          등업
+        </Button>
+      ),
+    },
+  ];
   if (isLoading || isError) {
     return <></>;
   }
